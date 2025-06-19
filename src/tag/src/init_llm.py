3d66c265991802b99065e9c4bfdbfccd71162ec9
Ilhammzz <ilham.abdulaziz20@gmail.com>
2025-06-19 15:15:44 +0700

    Refactor API call in test_run notebook: replace Gemini API with Anthropic client and update execution count

--- src/tag/src/init_llm.py
+++ src/tag/src/init_llm.py
@@ -1,19 +1,20 @@
 import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_ollama import ChatOllama
+from langchain_anthropic import ChatAnthropic
 from google.colab import userdata
 
-def init_llm(mode: str = "gemini"):
-    if mode == "gemini":
-        api_key = os.getenv("GEMINI_API_TOKEN")
+def init_llm(mode: str = "claude"):
+    if mode == "claude":
+        api_key = os.getenv("CLAUDE_API_TOKEN")
         if not api_key:
-            raise ValueError("API Key GEMINI tidak ditemukan.")
-        return ChatGoogleGenerativeAI(
-            model="gemini-2.0-flash",
+            raise ValueError("API Key tidak ditemukan.")
+        return ChatAnthropic(
+            model="claude-3-5-haiku-20241022",
             temperature=0.0,
-            top_p=1,
-            google_api_key=api_key,
-            timeout=60
+            max_tokens_to_sample=4096,
+            api_key=api_key,
+            timeout=None
         )
     elif mode == "evaluator":
         api_key = userdata.get("GOOGLE_API_KEY")
@@ -28,4 +29,4 @@ def init_llm(mode: str = "gemini"):
     elif mode == "ollama":
         return ChatOllama(model="llama3.1:8b-instruct-q4_K_M")
     else:
-        raise ValueError("Mode LLM tidak dikenali. Gunakan 'gemini' atau 'ollama'.")
+        raise ValueError("Mode LLM tidak dikenali. Gunakan 'claude' atau 'ollama'.")
